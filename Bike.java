c42af49baf4e506b783beeddd0de281c5c6c4669
shivani yadav <112896644+shivaniyuvrajyadav@users.noreply.github.com>
2022-12-17 22:19:58 +0530

    Add files via upload

---
+++ Bike.java
@@ -0,0 +1,12 @@
+class Bike 
+{
+	public static void main(String[] args) 
+	{
+		Bike ref1 = new Bike();
+        Bike ref2 = new Bike();
+        Bike ref3 = new Bike();
+		System.out.println(ref1);
+		System.out.println(ref2);
+		System.out.println(ref3);
+	}
+}
